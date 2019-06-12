0dc90264b7c5bff80b790d0672b4991c1f9ed708
isaach1 <isaachudis1@gmail.com>
2019-06-12 15:46:48 +0100

    Adding heatmap

--- heatmap/src/app/app.component.ts
+++ heatmap/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { ViewEncapsulation } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'my-app', 
+  template: `<ejs-heatmap id='heatmap-container'></ejs-heatmap>`,
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
-  title = 'heatmap';
+  // title = 'heatmap';
 }
--- heatmap/src/app/app.module.ts
+++ heatmap/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HeatMapModule} from '@syncfusion/ej2-angular-heatmap';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeatMapModule
   ],
   providers: [],
   bootstrap: [AppComponent]
