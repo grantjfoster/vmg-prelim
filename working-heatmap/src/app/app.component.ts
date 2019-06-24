30b8a3faaec8be3ed0e19918d9513278f31d2743
isaach1 <isaachudis1@gmail.com>
2019-06-24 18:26:28 +0100

    Some bug fixes

--- working-heatmap/src/app/app.component.ts
+++ working-heatmap/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { google } from 'google-maps';
+
+declare var google: google;
 
 @Component({
   selector: 'app-root',
@@ -17,21 +20,22 @@ export class AppComponent {
   onMapLoad(mapInstance: google.maps.Map) {
     this.map = mapInstance;
 
-    const coords: google.maps.LatLng[] = [
-      new google.maps.LatLng(37.782, -122.447),
+    const coords = [
+      {location: new google.maps.LatLng(37.782, -122.447), weight: 0.5},
       new google.maps.LatLng(37.782, -122.445),
-      new google.maps.LatLng(37.782, -122.443),
-      new google.maps.LatLng(37.782, -122.441),
-      new google.maps.LatLng(37.782, -122.439),
+      {location: new google.maps.LatLng(37.782, -122.443), weight: 2},
+      {location: new google.maps.LatLng(37.782, -122.441), weight: 3},
+      {location: new google.maps.LatLng(37.782, -122.439), weight: 2},
       new google.maps.LatLng(37.782, -122.437),
-      new google.maps.LatLng(37.782, -122.435),
-      new google.maps.LatLng(37.785, -122.447),
-      new google.maps.LatLng(37.785, -122.445),
+      {location: new google.maps.LatLng(37.782, -122.435), weight: 0.5},
+    
+      {location: new google.maps.LatLng(37.785, -122.447), weight: 3},
+      {location: new google.maps.LatLng(37.785, -122.445), weight: 2},
       new google.maps.LatLng(37.785, -122.443),
-      new google.maps.LatLng(37.785, -122.441),
+      {location: new google.maps.LatLng(37.785, -122.441), weight: 0.5},
       new google.maps.LatLng(37.785, -122.439),
-      new google.maps.LatLng(37.785, -122.437),
-      new google.maps.LatLng(37.785, -122.435)
+      {location: new google.maps.LatLng(37.785, -122.437), weight: 2},
+      {location: new google.maps.LatLng(37.785, -122.435), weight: 3}
     ]
     this.heatmap = new google.maps.visualization.HeatmapLayer({
       map: this.map,
